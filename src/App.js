b0498e8789a3fc063caf3a261fefef175aed0b27
Kally <kallyasmedia@gmail.com>
2020-07-19 13:26:23 +0000

    Update components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import PageLoader from "./Components/PageLoader";
 import Header from "./Components/Header";
+import PageCover from "./Components/PageCover";
+import MainContent from "./Components/MainContent";
 
 function App() {
   return (
     <div>
       <PageLoader />
       <Header />
+      <PageCover />
+      <MainContent />
     </div>
   );
 }
